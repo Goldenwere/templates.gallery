b2671dac50affa9a70019f163126faa95ab1f207
Lightling <contact@lightling.xyz>
2023-04-13 17:54:11 -0400

    Create GalleryArticle.vue

---
+++ src/components/embeds/GalleryArticle.vue
@@ -0,0 +1,22 @@
+<script setup lang='ts'>
+import { computed } from 'vue'
+import { marked } from 'marked'
+
+const props = defineProps<{
+  content: string
+}>()
+
+const parsed = computed(() => {
+  return marked.parse(props.content.replace(/^[\u200B\u200C\u200D\u200E\u200F\uFEFF]/,''))
+})
+</script>
+
+<template lang='pug'>
+article(
+  v-html='parsed'
+)
+</template>
+
+<style scoped lang='sass'>
+
+</style>
