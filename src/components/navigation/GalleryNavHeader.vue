e003452966622a63a5c1ff101dee348cac827e19
Lightling <contact@lightling.xyz>
2023-04-17 17:24:15 -0400

    define header

---
+++ src/components/navigation/GalleryNavHeader.vue
@@ -0,0 +1,40 @@
+<script setup lang='ts'>
+import { reactive } from 'vue'
+import { useStore } from '@/src/store'
+
+const store = useStore()
+const site = reactive(store.site)
+</script>
+
+<template lang='pug'>
+header
+  img(
+    :src='site.logo'
+  )
+  nav
+    router-link(
+      :to='{ name: "home" }'
+    ) 
+      span Home
+</template>
+
+<style scoped lang='sass'>
+header
+  width: 100%
+  height: 4em
+  background-color: var(--theme-nav-bg)
+  display: flex
+  align-items: center
+  img
+    height: 3em
+    margin: auto 0 auto 0.5em
+  nav
+    margin-left: 0.5em
+    display: flex
+    font-size: 2em
+    a
+      color: var(--theme-nav-fg)
+      text-decoration: none
+      font-weight: bold
+      font-family: var(--theme-font-display)
+</style>
--- src/main.vue
+++ src/main.vue
@@ -1,19 +1,25 @@
 <script setup lang='ts'>
-import { nextTick } from 'vue'
+import { computed, nextTick, ref } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 import { capitalizeFirstLetter } from '@/src/utilities/string'
 import { fetchAndParseYaml } from '@/src/utilities/fetch'
 import { useStore } from '@/src/store'
 import type site from '@/src/types/views/site'
+import GalleryNavHeader from './components/navigation/GalleryNavHeader.vue'
 
 const store = useStore()
 const route = useRoute()
 const router = useRouter()
+const ready = ref(false)
+const showHeader = computed(() => {
+  return ready && route.name !== 'home'
+})
 
 fetchAndParseYaml('/content/site.yml')
   .then(async (content) => {
     store.$patch({ site: content as site })
     await router.isReady()
+    ready.value = true
     document.title = `${capitalizeFirstLetter(route.name as string)} - ${store.site.title}`
     router.afterEach((to, from) => {
       nextTick(() => {
@@ -25,6 +31,9 @@ fetchAndParseYaml('/content/site.yml')
 
 <template lang='pug'>
 #entry
+  GalleryNavHeader(
+    v-if='showHeader'
+  )
   main
     router-view
 </template>
