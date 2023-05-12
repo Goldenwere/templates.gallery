c14a31a3f233d7a9a689dc93c9441535fa7fb6db
Lightling <contact@lightling.xyz>
2023-05-12 18:01:13 -0400

    add gallery directory nav to header

--- src/components/navigation/GalleryNavHeader.vue
+++ src/components/navigation/GalleryNavHeader.vue
@@ -1,13 +1,16 @@
 <script setup lang='ts'>
-import { reactive, watch } from 'vue'
+import { reactive, ref, watch } from 'vue'
 import { useStore } from '@/src/store'
+import type directoryRoute from '@/src/types/directoryRoute'
 
 const store = useStore()
 const site = reactive(store.site)
+const galleryDropdownOpen = ref(false)
 
 const headerData = reactive({
   hasCommissions: false,
   hasTos: false,
+  galleries: [] as any[],
 })
 
 if (site.directories !== undefined) {
@@ -23,6 +26,22 @@ if (site.directories !== undefined) {
 function initialize() {
   headerData.hasCommissions = !!site.directories?.find((other) => other.path === 'commission')
   headerData.hasTos = !!site.directories?.find((other) => other.path === 'tos')
+  headerData.galleries = site.directories.filter((other) => !['tos', 'commission'].includes(other.path)).map((other) => {
+    return {
+      route: {
+        name: 'gallery',
+        params: {
+          id: other.path,
+        }
+      },
+      ...other,
+    }
+  })
+}
+
+function onToggleMenu(event: Event, isOpen: boolean) {
+  event.preventDefault()
+  galleryDropdownOpen.value = isOpen
 }
 </script>
 
@@ -47,6 +66,17 @@ header
       v-if='headerData.hasTos'
       :to='{ name: "tos" }'
     ) Terms of Service
+    span.dropdown(
+      @mouseover='onToggleMenu($event, true)'
+      @mouseleave='onToggleMenu($event, false)'
+    ) Galleries
+      .menu(
+        v-if='galleryDropdownOpen'
+      )
+        router-link(
+          v-for='gallery in headerData.galleries'
+          :to='gallery.route'
+        ) {{ gallery.title || gallery.path }}
 </template>
 
 <style scoped lang='sass'>
@@ -70,9 +100,27 @@ header
     display: flex
     font-size: 2em
     gap: 1em
-    a
+    a,
+    .dropdown
       color: var(--theme-nav-fg)
       text-decoration: none
       font-weight: bold
       font-family: var(--theme-font-display)
+    .dropdown
+      position: relative
+      cursor: pointer
+      .menu
+        position: absolute
+        display: flex
+        flex-direction: column
+        height: auto
+        width: 8em
+        padding: 1em
+        background-color: black
+        font-size: 0.9em
+        z-index: 1
+        a
+          margin-bottom: 0.5em
+          &:last-child
+            margin-bottom: 0.25em
 </style>
