9923d007ce53e8012454dcd55389648a005dab03
Lightling <contact@lightling.xyz>
2023-05-12 00:17:26 -0400

    Add commissions/tos to header

--- src/components/navigation/GalleryNavHeader.vue
+++ src/components/navigation/GalleryNavHeader.vue
@@ -1,9 +1,21 @@
 <script setup lang='ts'>
-import { reactive } from 'vue'
+import { reactive, watch } from 'vue'
 import { useStore } from '@/src/store'
 
 const store = useStore()
 const site = reactive(store.site)
+
+const headerData = reactive({
+  hasCommissions: false,
+  hasTos: false,
+})
+
+watch(site, (oldSite, newSite) => {
+  if (newSite.directories !== undefined) {
+    headerData.hasCommissions = !!site.directories?.find((other) => other.path === 'commission')
+    headerData.hasTos = !!site.directories?.find((other) => other.path === 'tos')
+  }
+})
 </script>
 
 <template lang='pug'>
@@ -16,6 +28,14 @@ header
       :to='{ name: "home" }'
     ) 
       span Home
+    router-link(
+      v-if='headerData.hasCommissions'
+      :to='{ name: "commission" }'
+    ) Commission
+    router-link(
+      v-if='headerData.hasTos'
+      :to='{ name: "tos" }'
+    ) Terms of Service
 </template>
 
 <style scoped lang='sass'>
@@ -25,6 +45,8 @@ header
   background-color: var(--theme-nav-bg)
   display: flex
   align-items: center
+  position: relative
+  z-index: 99
   img
     height: 3em
     margin: auto 0 auto 0.5em
@@ -32,6 +54,7 @@ header
     margin-left: 0.5em
     display: flex
     font-size: 2em
+    gap: 1em
     a
       color: var(--theme-nav-fg)
       text-decoration: none
