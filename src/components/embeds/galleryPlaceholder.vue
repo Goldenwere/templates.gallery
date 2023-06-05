f3d0c7a80c417feabce1f8293ae0c5d73e0b0127
Lightling <contact@lightling.xyz>
2023-06-05 19:53:41 -0400

    create an image placeholder

---
+++ src/components/embeds/galleryPlaceholder.vue
@@ -0,0 +1,22 @@
+<script setup lang='ts'>
+defineProps<{
+  text?: string,
+}>()
+</script>
+
+<template lang='pug'>
+.placeholder
+  span {{ text || 'No Image' }}
+</template>
+
+<style scoped lang='sass'>
+.placeholder
+  display: flex
+  padding: 0.5em
+  background-color: var(--theme-placeholder-bg)
+  align-items: center
+  justify-content: center
+  span
+    color: var(--theme-placeholder-fg)
+    text-align: center
+</style>
