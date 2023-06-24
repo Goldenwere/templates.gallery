4648e060dc89459e90239aa43aa12bee0af68b3c
Lightling <contact@lightling.xyz>
2023-06-23 22:35:03 -0400

    use rfdc instead of json.stringify/json.parse

--- src/utilities/object.ts
+++ src/utilities/object.ts
@@ -1,3 +1,11 @@
+import rfdc from 'rfdc'
+
+const copy = rfdc()
+
+/**
+ * Creates a deep cloned copy of an object
+ * @param toCopy the object to copy
+ */
 export const deepCopy = <T>(toCopy: T) => {
-  return JSON.parse(JSON.stringify(toCopy)) as T
+  return copy(toCopy)
 }
